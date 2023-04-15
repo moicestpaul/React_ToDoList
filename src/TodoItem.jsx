3f8fc379bc1d1e8601deadc66d6ba86a758f0911
moicestpaul <bouloc.p@gmail.com>
2023-04-15 14:43:35 +0200

    fix trash

--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -1,14 +1,16 @@
 export function TodoItem({ completed, id, title, toggleTodo, deleteTodo }) {
   return (
     <li>
+      <div>
         <input
           type="checkbox"
           checked={completed}
           onChange={e => toggleTodo(id, e.target.checked)}
-          id={"checkbox-"+id}
+          id={"checkbox-" + id}
         />
-        <label htmlFor={"checkbox-"+id}></label>
+        <label htmlFor={"checkbox-" + id}></label>
         <span>{title}</span>
+      </div>
       <button onClick={() => deleteTodo(id)} className="btn btn-danger"></button>
     </li>
   )
